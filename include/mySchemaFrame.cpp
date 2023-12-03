7dc56a1f3667bfc9a7425a2c83b75031872c775a
Boyan Pang <127031897+Pepephant@users.noreply.github.com>
2023-12-03 16:12:01 +0800

    Delete include directory

--- include/mySchemaFrame.cpp
+++
@@ -1,7 +0,0 @@
-#include "mySchemaFrame.h"
-
-
-void MySchemaFrame :: OnImportButtonClick(wxCommandEvent& event) {
-    ComponentBrowser componentBrowser(this, "µ¯³ö¿ò", this->myPanel);
-    componentBrowser.ShowModal();
-}
